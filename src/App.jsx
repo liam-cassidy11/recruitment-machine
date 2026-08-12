c817d8ff10250ecfc6e8526d734557c7309eb6c8
Liam Cassidy <lcassid413@gmail.com>
2026-08-12 19:01:25 -0400

    fixed syntax error (lowercase instead of uppercase for page names)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Landing from "./pages/landing";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import ForgotPassword from "./pages/Forgotpassword";
+import ResetPassword from "./pages/Resetpassword";
+import Dashboard from "./pages/dashboard";
 import AthleteProfile from "./pages/profile";
-import Onboarding from "./components/onboarding/Onboarding"; // from your existing onboarding folder
+import Onboarding from "./components/onboarding/onboarding"; // from your existing onboarding folder
 
 export default function App() {
   return (
